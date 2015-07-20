525d2aca31ba5395e3aeb333ad0217db62c3764b
Dustyn <djtubbs1@svsu.edu>
2015-07-20 13:42:12 -0500

    Dont need these

--- init.lua
+++ init.lua
@@ -1,20 +1,9 @@
 local torch = require 'torch'
-local mpi = require 'mpi'
+mpi = require 'mpi'
 
 local distnn = {}
 
 distnn.hellomodule = require 'distnn.hellomodule'
 distnn.Input = require 'distnn.Input'
-
-
-
-
-
-
-torch.Tensor.mpi = function(self)
-print("IT WORKED")
-print(self)
-print("IT WORKED")
-end
 
 return distnn
