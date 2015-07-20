950161cd5935bb5ee6557ce22604068dd64cc121
Dustyn <djtubbs1@svsu.edu>
2015-07-20 14:15:16 -0500

    Just testing to see if 5 persists, it does

--- test/test.lua
+++ test/test.lua
@@ -5,7 +5,18 @@ torch = require 'torch'
 --example of creating a new input object
 labels = torch.Tensor(100)
 
-input = distnn.Input.new(torch.Tensor(32,32,3,100):zero(),torch.Tensor(100):zero())
-print(input)
+t = torch.Tensor(32,32,3,100):zero()
+
+t[1][1][1][1] = 5
+
+input = distnn.Input.new(t,torch.Tensor(100):zero())
+
+
+for key,value in pairs(input.distributed_data) do 
+  if(key == 0) then
+    print(value:select(1,1))
+  end
+end
+
 
 mpi.finalize()
